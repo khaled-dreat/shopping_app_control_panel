eb98032a02afead1872c6c77c2ecf08ad5c9938f
khaled-dreat <dreatkhaled@gmail.com>
2024-03-31 22:38:48 +0300

    Add functionality to upload images and add products remotely

--- lib/features/products/data/datasources/products_remote_data_source.dart
+++ lib/features/products/data/datasources/products_remote_data_source.dart
@@ -11,9 +11,11 @@ class ProductsReomteDataSourceImpl extends ProductsReomteDataSource {
 
   @override
   Future<void> addProduct(ProductDataEntitie productEntitie) async {
+    // * Add Assets & Return List of Assets ID
     List<String> uploadedImageIds = await apiService.postAddAssets(
       endPoint: ApiKey.urlBulder(ApiKey.assets),
     );
+    // * Add Product & Return Pruduct ID
     String productID = await apiService.postAddProduct(
         endPoint: ApiKey.urlBulder(ApiKey.products),
         data: productEntitie.toJson());
